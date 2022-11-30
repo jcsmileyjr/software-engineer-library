f193b555766226e8e2e5ad0b3bb577cce742d560
Smiley <jcsmileyjr@gmail.com>
2022-11-29 20:30:59 -0600

    Feature: Add the detail icon to the book component

--- client/src/components/book/Book.js
+++ client/src/components/book/Book.js
@@ -1,8 +1,12 @@
 import "./book.css";
+import Details from "../../assets/details-library.png";
 
 const Book = () => {
     return(
         <div className="book--container">
+            <div className="book__title--container">
+                <img className="book__detailIcon--style" src={Details} alt="Hover or click to display more options" />
+            </div>
             <p className="book__title--style">React Testing Library – Tutorial with JavaScript Code Examples</p>
             <hr />
             <p>Nathan Sebhastian</p>
