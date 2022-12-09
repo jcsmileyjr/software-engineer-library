e06a05b74864410e4626dd753d622d452dc86a80
Smiley <jcsmileyjr@gmail.com>
2022-12-09 17:17:43 -0600

    Refactor Bug: Netlify issue of file name

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/header/Header";
+import Header from "./components/header/header";
 import Content from "./components/content/Content";
 import MobileTools from "./components/mobileTools/MobileTools";
 import "./App.css";
